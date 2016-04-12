b9f87020f5b5eb5ba6f21b12f89c099e7db9dd64
Damon <damon.frankfurth@gmail.com>
2016-04-11 20:45:19 -0600

    Add translation motion to flip animation. Also addded a check to ensure animation couldn't be triggered again by clicking the same tab header that user was currently on.

--- static/js/app.js
+++ static/js/app.js
@@ -2,34 +2,40 @@
 	var app = angular.module('flatter', []);
 	app.controller('tabManager', ['$scope', function($scope){
 		this.tab = 1;
-		$scope.curScale = 1;
 		$scope.animDiv = document.getElementById("animDiv");
-		$scope.scalOut = true;
+		$scope.curScale = 1;
+		$scope.curTrans = 0;
+		$scope.flipOut = true;
 		$scope.interval = null;
 
 		this.selectTab = function(newTab){
-			this.tab = newTab;
-			$scope.interval = setInterval(this.flipAnim, 20);
+			if (this.tab != newTab){
+				this.tab = newTab;
+				$scope.interval = setInterval(this.flipAnim, 20);
+			}
 		};
 		this.isSelected = function(checkT){
 			return this.tab === checkT;
 		};
 		this.flipAnim = function(){
-			if ($scope.scalOut){
+			if ($scope.flipOut){
 				$scope.curScale -= 0.05;
+				$scope.curTrans += 2;
 				if ($scope.curScale <= 0.5){
-					$scope.scalOut = false;
+					$scope.flipOut = false;
 				}
 			} else {
 				$scope.curScale += 0.05;
+				$scope.curTrans -= 2;	
 				if ($scope.curScale >= 1){
 					$scope.curScale = 1;
-					$scope.scalOut = true;
+					$scope.curTrans = 0;
+					$scope.flipOut = true;
 					clearInterval($scope.interval);
 				}
 			}
 
-			$scope.animDiv.style.transform = "scale3d(" + $scope.curScale + ", " + $scope.curScale + ", " + $scope.curScale +")";
+			$scope.animDiv.style.transform = "scale3d(" + $scope.curScale + ", " + $scope.curScale + ", " + $scope.curScale +") translateY(-" + $scope.curTrans + "vw)";
 		};
 	}]);
 	app.controller('orderController', [ '$http', function($http){
