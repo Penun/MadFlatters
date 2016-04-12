7a5625c793807a236e5cdc93089aecc98e7aaae4
Damon <damon.frankfurth@gmail.com>
2016-04-12 14:11:04 -0600

    Added x translate and a case handling for max-width.

--- static/js/app.js
+++ static/js/app.js
@@ -4,11 +4,18 @@
 		this.tab = 1;
 		$scope.animDiv = document.getElementById("animDiv");
 		$scope.curScale = 1;
-		$scope.curTrans = 0;
+		$scope.curTransY = 0;
+		$scope.curTransX = 0;
 		$scope.curDeg = 0;
 		$scope.flipOut = true;
 		$scope.flipRight = true;
 		$scope.interval = null;
+
+		if (document.documentElement.clientWidth >= 1366){
+			$scope.cappedWidth = true;
+		} else {
+			$scope.cappedWidth = false;
+		}
 
 		this.selectTab = function(newTab){
 			if (this.tab != newTab){
@@ -22,39 +29,69 @@
 		this.flipAnim = function(){
 			if ($scope.flipOut){
 				$scope.curScale -= 0.05;
-				$scope.curTrans += 1.5;
+				$scope.curTransY += 1.5;
 				if ($scope.flipRight){
 					$scope.curDeg += 9;
+					if ($scope.cappedWidth){
+						$scope.curTransX -= 68.3;
+					} else {
+						$scope.curTransX -= 5;
+					}
 				} else {
 					$scope.curDeg -= 9;
+					if ($scope.cappedWidth){
+						$scope.curTransX += 68.3;
+					} else {
+						$scope.curTransX += 5;
+					}
 				}
 				if ($scope.curScale <= 0.5){
 					$scope.flipOut = false;
 				}
 			} else {
 				$scope.curScale += 0.05;
-				$scope.curTrans -= 1.5;
+				$scope.curTransY -= 1.5;
 				if ($scope.flipRight){
 					$scope.curDeg += 9;
+					if ($scope.cappedWidth){
+						$scope.curTransX -= 68.3;
+					} else {
+						$scope.curTransX -= 5;
+					}
 				} else {
 					$scope.curDeg -= 9;
+					if ($scope.cappedWidth){
+						$scope.curTransX += 68.3;
+					} else {
+						$scope.curTransX += 5;
+					}
 				}
 				if ($scope.curScale >= 1){
 					$scope.curScale = 1;
-					$scope.curTrans = 0;
+					$scope.curTransY = 0;
 					if ($scope.flipRight){
 						$scope.curDeg = 180;
 						$scope.flipRight = false;
+						if ($scope.cappedWidth){
+							$scope.curTransX = -1366;
+						}
 					} else {
 						$scope.curDeg = 0;
 						$scope.flipRight = true;
+						if ($scope.cappedWidth){
+							$scope.curTransX = 0;
+						}
 					}
 					$scope.flipOut = true;
 					clearInterval($scope.interval);
 				}
 			}
 
-			$scope.animDiv.style.transform = "scale3d(" + $scope.curScale + ", " + $scope.curScale + ", " + $scope.curScale +") translateY(-" + $scope.curTrans + "vw) rotateY(" + $scope.curDeg + "deg)";
+			if ($scope.cappedWidth){
+				$scope.animDiv.style.transform = "scale3d(" + $scope.curScale + ", " + $scope.curScale + ", 1) translate3d(" + $scope.curTransX + "px, -" + $scope.curTransY + "vw, 0) rotateY(" + $scope.curDeg + "deg)";				
+			} else {
+				$scope.animDiv.style.transform = "scale3d(" + $scope.curScale + ", " + $scope.curScale + ", 1) translate3d(" + $scope.curTransX + "vw, -" + $scope.curTransY + "vw, 0) rotateY(" + $scope.curDeg + "deg)";
+			}
 		};
 	}]);
 	app.controller('orderController', [ '$http', function($http){
