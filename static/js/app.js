bc1c06e9dc9bea30d741fe80031eff7874153910
Damon <damon.frankfurth@gmail.com>
2016-04-12 10:45:22 -0600

    Basic 180 rotation setup.

--- static/js/app.js
+++ static/js/app.js
@@ -5,13 +5,15 @@
 		$scope.animDiv = document.getElementById("animDiv");
 		$scope.curScale = 1;
 		$scope.curTrans = 0;
+		$scope.curDeg = 0;
 		$scope.flipOut = true;
+		$scope.flipRight = true;
 		$scope.interval = null;
 
 		this.selectTab = function(newTab){
 			if (this.tab != newTab){
 				this.tab = newTab;
-				$scope.interval = setInterval(this.flipAnim, 20);
+				$scope.interval = setInterval(this.flipAnim, 60);
 			}
 		};
 		this.isSelected = function(checkT){
@@ -20,22 +22,39 @@
 		this.flipAnim = function(){
 			if ($scope.flipOut){
 				$scope.curScale -= 0.05;
-				$scope.curTrans += 2;
+				$scope.curTrans += 1.5;
+				if ($scope.flipRight){
+					$scope.curDeg += 9;
+				} else {
+					$scope.curDeg -= 9;
+				}
 				if ($scope.curScale <= 0.5){
 					$scope.flipOut = false;
 				}
 			} else {
 				$scope.curScale += 0.05;
-				$scope.curTrans -= 2;	
+				$scope.curTrans -= 1.5;
+				if ($scope.flipRight){
+					$scope.curDeg += 9;
+				} else {
+					$scope.curDeg -= 9;
+				}
 				if ($scope.curScale >= 1){
 					$scope.curScale = 1;
 					$scope.curTrans = 0;
+					if ($scope.flipRight){
+						$scope.curDeg = 180;
+						$scope.flipRight = false;
+					} else {
+						$scope.curDeg = 0;
+						$scope.flipRight = true;
+					}
 					$scope.flipOut = true;
 					clearInterval($scope.interval);
 				}
 			}
 
-			$scope.animDiv.style.transform = "scale3d(" + $scope.curScale + ", " + $scope.curScale + ", " + $scope.curScale +") translateY(-" + $scope.curTrans + "vw)";
+			$scope.animDiv.style.transform = "scale3d(" + $scope.curScale + ", " + $scope.curScale + ", " + $scope.curScale +") translateY(-" + $scope.curTrans + "vw) rotateY(" + $scope.curDeg + "deg)";
 		};
 	}]);
 	app.controller('orderController', [ '$http', function($http){
